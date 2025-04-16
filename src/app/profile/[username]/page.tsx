0285c553c3a4a6832d77e9d37cbeed0f7e5633f0
Ing3rulDrogat <myta.boss39@yahoo.com>
2025-04-16 12:42:11 +0000

    Fixed build issues. Ready for launch

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -7,23 +7,28 @@ import {
 import { notFound } from "next/navigation";
 import React from "react";
 import ProfilePageClient from "./ProfilePageClient";
+import type { Metadata } from "next";
 
-export async function generateMetaData({
-  params,
-}: {
-  params: { username: string };
-}) {
-  const user = await getProfileByUsername(params.username);
-  if (!user) return;
+// export async function generateMetadata({ params }: Props) {
+//   const user = await getProfileByUsername(params.username);
+//   if (!user) return;
 
-  return {
-    title: `${user.name ?? user.username}`,
-    description: user.bio || `Check out ${user.username}'s profile`,
-  };
+//   return {
+//     title: `${user.name ?? user.username}`,
+//     description: user.bio || `Check out ${user.username}'s profile`,
+//   };
+// }
+
+interface Props {
+  params: Promise<{
+    username: string;
+  }>;
 }
 
-async function ProfilePage({ params }: { params: { username: string } }) {
-  const user = await getProfileByUsername(params.username);
+async function ProfilePage({ params }: Props) {
+  const { username } = await params; // Await the promise to get the actual `username`
+  const user = await getProfileByUsername(username);
+
   if (!user) notFound();
 
   const [posts, likedPosts, isCurrentUserFollowing] = await Promise.all([
